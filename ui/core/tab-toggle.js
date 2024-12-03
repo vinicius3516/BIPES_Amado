4dba5c38921925fcca2312992d3fa5b0599d0351
Antonio Mortari <antoniomortari09@gmail.com>
2024-12-02 22:59:46 -0300

    esconder aba 'Databoard' ao selecionar AmadoBoard

--- ui/core/tab-toggle.js
+++ ui/core/tab-toggle.js
@@ -5,15 +5,20 @@ const verifyAmadoBoardIsSelected = () => {
   const contentSound = document.getElementById("containerSound");
 
   const tabShared = document.getElementById("tab_programs");
+  const tabDataboard = document.getElementById("tab_databoard");
 
   if (valueDeviceSelector === "AmadoBoard") {
     tabSound.style.display = "block";
     contentSound.style.display = "block";
+
     tabShared.style.display = "none";
+    tabDataboard.style.display = "none";
   } else {
     tabSound.style.display = "none";
     contentSound.style.display = "none";
+
     tabShared.style.display = "block";
+    tabDataboard.style.display = "block";
   }
 };
 
